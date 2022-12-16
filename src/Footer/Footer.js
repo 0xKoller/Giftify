14c7e6303024221b98d37ea68863e59e610a8f2f
Jose Luis Koller <joseluiskoller98@gmail.com>
2022-12-15 23:59:16 -0300

    Assets Folder

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -49,7 +49,7 @@ function Footer() {
             target="_blank"
           >
             <img
-              srcset="https://cdn.cafecito.app/imgs/buttons/button_5.png 1x, https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x"
+              srcSet="https://cdn.cafecito.app/imgs/buttons/button_5.png 1x, https://cdn.cafecito.app/imgs/buttons/button_5_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_5_3.75x.png 3.75x"
               src="https://cdn.cafecito.app/imgs/buttons/button_5.png"
               alt="Invitame un café en cafecito.app"
             />
